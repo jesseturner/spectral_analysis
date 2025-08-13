106a8b826f2ef64539316bfebb4b5930fbfbcabf
Jesse Turner <jturner@jpss-cloud6.cira.colostate.edu>
2025-08-13 21:25:30 +0000

    Reading tp7 with pandas fixed-width file reader

--- spectral_utils/spectral_utils.py
+++ spectral_utils/spectral_utils.py
@@ -4,44 +4,14 @@ from io import StringIO
 
 
 #--- MODTRAN utils
+
 def open_tp7_file(filepath):
-    with open(filepath, 'r') as f:
-        lines = f.readlines()
-    
-    #--- Find the header line index
-    header_idx = None
-    for i, line in enumerate(lines):
-        if line.strip().startswith("FREQ"):
-            header_idx = i
-            break
-    
-    if header_idx is None:
-        raise ValueError("No header line starting with 'FREQ' found.")
-    
-    headers = lines[header_idx].split()
-    lines = lines[header_idx+1:]
-    
 
-    processed_lines = []
-    for line in lines[:50]:
+    #--- Set up for a fixed-width file
+    col_names = ['FREQ', 'TOT_TRANS', 'THRML_EM', 'THRML_SCT', 'SURF_EMIS', 'MULT_SCAT', 
+        'SING_SCAT', 'GRND_RFLT', 'DRCT_RFLT', 'TOTAL_RAD', 'REF_SOL', 'SOL@OBS', 'DEPTH', 
+        'DIR_EM', 'TOA_SUN', 'BBODY_T[K]']
+    col_widths = [8, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 9, 6, 7, 11, 11]
 
-        line = line.rstrip("\n")
-        line = line.lstrip()
-        line_og = line
-
-        line = line_og[:50] + "  " + line_og[72:83] + " " + line_og[94:105] + "  " + line_og[124:138] + " " + line_og[152:]
-        
-        line = re.sub(" ", "|", line)
-        processed_lines.append(line)
-        print(line)
-
-    df = pd.read_csv(
-        StringIO("\n".join(processed_lines)),
-        sep='|',
-        engine='python',
-        on_bad_lines="warn",
-        names=headers, 
-        header=None
-    )
-        
+    df = pd.read_fwf(filepath, widths=col_widths, names=col_names, skiprows=11)
     return df
