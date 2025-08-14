3cada4560afebb8de453ef8af4a4a47626087a70
Jesse Turner <jturner@jpss-cloud6.cira.colostate.edu>
2025-08-14 15:59:41 +0000

    Added plotting of radiance

--- main_cris.py
+++ main_cris.py
@@ -1,3 +1,11 @@
 from spectral_utils import cris_utils as c_utils
 
-c_utils.download_cris_data(date_start="2025-06-25", date_end="2025-06-25", lon_west=-105.31, lat_south=39.98, lon_east=-105.2, lat_north=40.062)
+#c_utils.download_cris_data(date_start="2025-06-25", date_end="2025-06-25", lon_west=-105.31, lat_south=39.98, lon_east=-105.2, lat_north=40.062)
+
+ds = c_utils.open_cris_data("CrIS_data/SNDR.J1.CRIS.20250625T0854.m06.g090.L1B.std.v03_08.G.250625154750.nc")
+
+ds = c_utils.isolate_target_point(ds, target_lat=42.27, target_lon=-66.25)
+
+c_utils.plot_radiance_spectra(ds, fig_dir="CrIS_plot", fig_name="CrIS_plot_rad")
+
+print(ds)
--- spectral_utils/cris_utils.py
+++ spectral_utils/cris_utils.py
@@ -1,4 +1,7 @@
 import earthaccess, os
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
 
 def download_cris_data(date_start, date_end, lon_west, lat_south, lon_east, lat_north, cris_dir="CrIS_data"):
     #--- Earthaccess docs: https://earthaccess.readthedocs.io/en/latest/quick-start/
@@ -18,4 +21,56 @@ def download_cris_data(date_start, date_end, lon_west, lat_south, lon_east, lat_
     )
     os.makedirs(f"{cris_dir}", exist_ok=True)
     files = earthaccess.download(results, cris_dir)
+    return
+
+def open_cris_data(filepath):
+    ds = xr.open_dataset(filepath)
+    return ds
+
+def isolate_target_point(ds, target_lat, target_lon):
+    abs_diff = np.abs(ds['lat'] - target_lat) + np.abs(ds['lon'] - target_lon).values
+    atrack_idx, xtrack_idx, fov_idx = np.unravel_index(abs_diff.argmin(), abs_diff.shape)
+    ds_target = ds.isel(atrack=atrack_idx, xtrack=xtrack_idx, fov=fov_idx)
+
+    print(f"Using lat/lon of {ds_target['lat'].values:.2f}, {ds_target['lon'].values:.2f}, fov of {fov_idx}")
+    return ds_target
+
+def planck_radiance(wnum, T):
+    
+    C1 = 1.191042722E-12		
+    C2 = 1.4387752			# units are [K cm]
+    C1 = C1 * 1e7			# units are now [mW/m2/ster/cm-4]
+    rad = C1 * wnum * wnum * wnum / (np.exp(C2 * wnum / T)-1)
+    
+    return rad
+
+def plot_radiance_spectra(ds, fig_dir="CrIS_plot", fig_name="CrIS_plot"):
+    fig = plt.figure(figsize=(10, 5))
+
+    wnum_lw = ds['wnum_lw'].values
+    wnum_mw = ds['wnum_mw'].values
+    wnum_sw = ds['wnum_sw'].values
+    radiance_lw = ds["rad_lw"]
+    radiance_mw = ds["rad_mw"]
+    radiance_sw = ds["rad_sw"]
+
+    plt.plot(wnum_lw, planck_radiance(wnum_lw, 290), color="blue", linewidth=1, label="290 K Blackbody")
+    plt.plot(wnum_mw, planck_radiance(wnum_mw, 290), color="blue", linewidth=1)
+    plt.plot(wnum_sw, planck_radiance(wnum_sw, 290), color="blue", linewidth=1)
+    plt.xlim(500, 2500)
+    plt.ylim(-5, 160)
+    plt.legend()
+
+    plt.plot(wnum_lw, radiance_lw, label="Longwave IR", color="black", linewidth=0.5)
+    plt.plot(wnum_mw, radiance_mw, label="Midwave IR", color="black", linewidth=0.5)
+    plt.plot(wnum_sw, radiance_sw, label="Shortwave IR", color="black", linewidth=0.5)
+
+    plt.xlabel("Wavenumber (cm⁻¹)")
+    plt.ylabel("Radiance (mW/m²/sr/cm⁻¹)")
+    plt.title(f"Infrared Spectrum from CrIS")
+    plt.grid(color='#d3d3d3')
+
+    os.makedirs(f"{fig_dir}", exist_ok=True)
+    fig.savefig(f"{fig_dir}/{fig_name}", dpi=200, bbox_inches='tight')
+    plt.close()
     return
