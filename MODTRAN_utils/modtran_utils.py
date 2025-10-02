65b4dd6ae45e6934bb5d4ee5403d731010baecfd
Jesse Turner <jturner@jpss-cloud6.cira.colostate.edu>
2025-10-02 19:03:41 +0000

    Logic for coarsening spectral resolution, requires using .7sc file

--- MODTRAN_utils/modtran_utils.py
+++ MODTRAN_utils/modtran_utils.py
@@ -10,14 +10,23 @@ import subprocess
 #--- MODTRAN utils
 
 def open_tp7_file(filepath):
-
     #--- Set up for a fixed-width file
     col_names = ['FREQ', 'TOT_TRANS', 'THRML_EM', 'THRML_SCT', 'SURF_EMIS', 'MULT_SCAT', 
         'SING_SCAT', 'GRND_RFLT', 'DRCT_RFLT', 'TOTAL_RAD', 'REF_SOL', 'SOL@OBS', 'DEPTH', 
         'DIR_EM', 'TOA_SUN', 'BBODY_T[K]']
     col_widths = [8, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 9, 6, 7, 11, 11]
-
     df = pd.read_fwf(filepath, widths=col_widths, names=col_names, skiprows=11)
+
+    return df
+
+def open_7sc_file(filepath):
+    #--- Set up for a fixed-width file
+    col_names = ['FREQ', 'TOT_TRANS', 'THRML_EM', 'THRML_SCT', 'SURF_EMIS', 'MULT_SCAT', 
+        'SING_SCAT', 'GRND_RFLT', 'DRCT_RFLT', 'TOTAL_RAD', 'REF_SOL', 'SOL@OBS', 'DEPTH', 
+        'DIR_EM', 'TOA_SUN', 'BBODY_T[K]']
+    col_widths = [12, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 9, 6, 7, 11, 11]
+    df = pd.read_fwf(filepath, widths=col_widths, names=col_names, skiprows=11,
+        skipfooter=1, engine="python")
 
     return df
 
