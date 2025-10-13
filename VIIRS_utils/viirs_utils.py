d5342a95704c9b02cf4839a69332b5baa42d87b0
Jesse Turner <jturner@jpss-cloud6.cira.colostate.edu>
2025-10-13 15:56:34 +0000

    Added point marker to viirs plots

--- VIIRS_utils/viirs_utils.py
+++ VIIRS_utils/viirs_utils.py
@@ -112,9 +112,10 @@ def _apply_scale_and_offset(file_path, band, da):
     return clean_da
 
 
-def plot_viirs_data(da, plot_dir, plot_name, plot_title, extent=None):
+def plot_viirs_data(da, plot_dir, plot_name, plot_title, extent=None, pin_coords=None):
     """
     extent: [west, east, south, north]
+    pin_coords: (latitude, longitude)
     """
     projection=ccrs.PlateCarree(central_longitude=0)
     fig,ax=plt.subplots(1, figsize=(12,12), subplot_kw={'projection': projection})
@@ -130,7 +131,15 @@ def plot_viirs_data(da, plot_dir, plot_name, plot_title, extent=None):
     clb = plt.colorbar(pcm, shrink=0.6, pad=0.02, ax=ax)
     clb.ax.tick_params(labelsize=15)
     clb.set_label('(K)', fontsize=15)
-    
+
+    if pin_coords:
+        ax.plot(pin_coords[1], pin_coords[0], 
+            marker='o', 
+            markersize=12,
+            markerfacecolor='red',
+            markeredgecolor='white',
+            markeredgewidth=1.5, 
+            transform=ccrs.PlateCarree())
     if extent: ax.set_extent(extent, crs=ccrs.PlateCarree())
     ax.set_title(plot_title, fontsize=20, pad=10)
     ax.coastlines(resolution='50m', color='black', linewidth=1)
