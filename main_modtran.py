3679030df7900f8587d5ca09f8adef14a0965f54
Jesse Turner <jturner@jpss-cloud6.cira.colostate.edu>
2025-10-28 19:24:39 +0000

    Running plotting

--- main_modtran.py
+++ main_modtran.py
@@ -51,16 +51,17 @@ df2 = m_utils.open_7sc_file("flc_custom1.7sc") #--- Necessary for adjusting reso
 # m_utils.plot_skew_t_from_profile(df, title="GFS and SST vertical profile (2025-03-12)", fig_dir="/home/jturner/spectral_analysis", fig_name="gfs_vertical_profile_20250312")
 
 #--- Plot brightness temperature with VIIRS SRFs
-band = "B07"
+description = "sw_zoomed"
 xlim = (3.6, 4.2)
-ylim = (274, 281)
+ylim = (270, 279)
 freq_range =[10000/xlim[1], 10000/xlim[0]]
 # srf_file0 = "spectral_response_functions/line_395.dat"
-# srf_file1 = "spectral_response_functions/NPP_VIIRS_NG_RSR_M15_filtered_Oct2011f_BA.dat"
-srf_file2 = "spectral_response_functions/GOES-R_ABI_SRF_ch7.dat"
-srf_file_list = [srf_file2]
-srf_name_list = ["B07"]
-color_list = ["#4A8FE7"]
+srf_file1 = "spectral_response_functions/NPP_VIIRS_NG_RSR_M13_filtered_Oct2011f_BA.dat"
+# srf_file2 = "spectral_response_functions/GOES-R_ABI_SRF_ch7.dat"
+srf_file3 = "spectral_response_functions/NPP_VIIRS_NG_RSR_M12_FAKE.dat"
+srf_file_list = [srf_file1, srf_file3]
+srf_name_list = ["M13", "M12 Fake"]
+color_list = ["#EDB88B", "#CD5334"]
 
 m_utils.plot_freq_range_srf(df2, srf_file_list, srf_name_list, color_list,
     fig_dir='MODTRAN_plot', fig_name=f'band{band}_{fig_name}', fig_title=f"MODTRAN {title_name} \n Profile from GFS and OISST",
