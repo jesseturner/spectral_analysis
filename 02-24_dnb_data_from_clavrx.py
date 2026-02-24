46f7d61d0844e11d253b6909d5a616dbadfd3259
Jesse Turner <jturner@jpss-cloud6.cira.colostate.edu>
2026-02-24 21:46:39 +0000

    Plotting multiple file clavrx cloud mask and DNB reflectance

--- 02-24_dnb_data_from_clavrx.py
+++ 02-24_dnb_data_from_clavrx.py
@@ -22,20 +22,23 @@ def main():
     clavrx_path = "/mnt/overcastnas1/LEO_clavrx/JPSS_global/"
     all_files = get_clavrx_files(clavrx_path, high_illum_dates)
 
-    orbit = "b42765"
+    print("Needs additional filter for nighttime...")
+
+    orbit = "b73957"
     orbit_files = [f for f in all_files if re.search(orbit, f)]
     print(f"{len(orbit_files)} files in orbit {orbit}...")
 
-    # sample_id = 6
-    # print(f"Setting sample to file {sample_id}...")
-    # da_cloud_mask, datetime_str = get_cloud_mask_single(orbit_files[sample_id])
-    # plot_clavrx_cloud_mask(da_cloud_mask, datetime_str, save_path="clavrx_cloud_mask_file")
-
-    sample_start, sample_end = 0, 12
+    sample_start, sample_end = 24, 36
     print(f"Setting sample to file {sample_start} to {sample_end}...")
     orbit_files_select = only_valid_files(orbit_files[sample_start:sample_end])
-    da_cloud_mask, datetime_str = get_cloud_mask_multiple(orbit_files_select)
-    plot_clavrx_cloud_mask(da_cloud_mask, datetime_str, save_path="clavrx_cloud_mask_multiple")
+    
+    ds_combined, datetime_str = get_combined_multiple_files(orbit_files_select)
+
+    da_refl_lunar = get_variable(ds_combined, 'refl_lunar_dnb_nom')
+    da_cloud_mask = get_variable(ds_combined, 'cloud_mask')
+    
+    plot_clavrx_cloud_mask(da_cloud_mask, datetime_str, save_path="clavrx_cloud_mask")
+    plot_clavrx_dnb_refl(da_refl_lunar, datetime_str, save_path="clavrx_refl_lunar")
 
     return
 
@@ -73,7 +76,7 @@ def moon_illumination(date):
         phase_angle = s.separation_from(m).degrees
 
         # Illumination fraction formula
-        illumination = (1 + np.cos(np.radians(phase_angle))) / 2
+        illumination = (1 - np.cos(np.radians(phase_angle))) / 2
         return illumination * 100
 
 def get_clavrx_files(clavrx_path, date_list):
@@ -94,35 +97,41 @@ def get_clavrx_files(clavrx_path, date_list):
     print(f"Total files with high moonlight: {len(all_files)}")
     return all_files
 
-def get_cloud_mask_single(file):
-    ds_example = xr.open_dataset(
-        file,
-        engine='netcdf4')
+def plot_clavrx_dnb_refl(da_dnb_refl, datetime_str, save_path):
+    print("Plotting clavr-x DNB reflectance...")
+    projection = ccrs.PlateCarree(central_longitude=0)
+    fig, ax = plt.subplots(1, figsize=(12,12),
+                        subplot_kw={'projection': projection})
 
-    match = re.search(r'd(\d{8})_t(\d{6})', file)
-    if match:
-        date_part = match.group(1)
-        time_part = match.group(2)
-        date_formatted = f"{date_part[:4]}-{date_part[4:6]}-{date_part[6:]}"
-        time_formatted = f"{time_part[:2]}:{time_part[2:4]}"
-        datetime_str = f"{date_formatted} {time_formatted}"
-    else:
-        datetime_str = "Unknown - File format unexpected."
+    step = 6
+    sc = ax.scatter(
+        da_dnb_refl["longitude"][::step],
+        da_dnb_refl["latitude"][::step],
+        c=da_dnb_refl[::step],
+        cmap="Blues_r",
+        s=1,
+        marker="s",
+        linewidths=0,
+        transform=ccrs.PlateCarree()
+    )
 
-    da_cloud_mask = ds_example['cloud_mask']
-    valid = np.isfinite(da_cloud_mask) & np.isfinite(da_cloud_mask["latitude"]) & np.isfinite(da_cloud_mask["longitude"])
-    da_cloud_mask = da_cloud_mask.where(valid, drop=True)
+    ax.set_title(f"Clavrx DNB Reflectance \n {datetime_str}", fontsize=20, pad=10)
+    ax.coastlines(resolution='50m', color='black', linewidth=1)
 
-    return da_cloud_mask, datetime_str
+    plt.savefig(f"plots/{save_path}.png",
+                dpi=200, bbox_inches='tight')
+    plt.close()
 
-def get_cloud_mask_multiple(orbit_files):
+    return
+
+def get_combined_multiple_files(orbit_files):
     ds_combined = xr.open_mfdataset(
         orbit_files,
         combine="nested",
         preprocess=fix_dims,
         concat_dim="scan_lines_along_track_direction",
         engine='netcdf4')
-
+    
     start_dt = extract_datetime_from_filename(orbit_files[0])
     end_dt   = extract_datetime_from_filename(orbit_files[-1])
     if start_dt and end_dt:
@@ -130,11 +139,7 @@ def get_cloud_mask_multiple(orbit_files):
     else:
         datetime_str = "Unknown - File format unexpected."
 
-    da_cloud_mask = ds_combined['cloud_mask']
-    valid = np.isfinite(da_cloud_mask) & np.isfinite(da_cloud_mask["latitude"]) & np.isfinite(da_cloud_mask["longitude"])
-    da_cloud_mask = da_cloud_mask.where(valid.compute(), drop=True)
-
-    return da_cloud_mask, datetime_str
+    return ds_combined, datetime_str
 
 def fix_dims(ds):
     ds = ds.drop_dims("FakeDim1D")
@@ -154,6 +159,17 @@ def extract_datetime_from_filename(filename):
 
     return f"{date_formatted} {time_formatted}"
 
+def only_valid_files(file_list):
+    good_files = []
+    for f in file_list:
+        try:
+            with xr.open_dataset(f, engine="netcdf4") as ds:
+                ds.load()
+            good_files.append(f)
+        except Exception as e:
+            print(f"Skipping corrupted file: {f} ({e})")
+    return good_files
+
 def plot_clavrx_cloud_mask(da_cloud_mask, datetime_str, save_path):
     print("Plotting clavr-x cloud mask...")
     projection = ccrs.PlateCarree(central_longitude=0)
@@ -170,22 +186,14 @@ def plot_clavrx_cloud_mask(da_cloud_mask, datetime_str, save_path):
     bounds = np.arange(-0.5, 4.5, 1)
     norm = BoundaryNorm(bounds, cloud_cmap.N)
 
-    # pcm = ax.pcolormesh(
-    #     da_cloud_mask["longitude"],
-    #     da_cloud_mask["latitude"],
-    #     da_cloud_mask,
-    #     cmap=cloud_cmap,
-    #     norm=norm,
-    #     transform=ccrs.PlateCarree()
-    # )
-    step = 9
+    step = 6
     sc = ax.scatter(
         da_cloud_mask["longitude"][::step],
         da_cloud_mask["latitude"][::step],
         c=da_cloud_mask[::step],
         cmap=cloud_cmap,
         norm=norm,
-        s=0.6,
+        s=1,
         marker="s",
         linewidths=0,
         transform=ccrs.PlateCarree()
@@ -214,16 +222,13 @@ def plot_clavrx_cloud_mask(da_cloud_mask, datetime_str, save_path):
 
     return
 
-def only_valid_files(file_list):
-    good_files = []
-    for f in file_list:
-        try:
-            with xr.open_dataset(f, engine="netcdf4") as ds:
-                ds.load()
-            good_files.append(f)
-        except Exception as e:
-            print(f"Skipping corrupted file: {f} ({e})")
-    return good_files
+def get_variable(ds, var_name):
+
+    da_var = ds[var_name]
+    valid = np.isfinite(da_var) & np.isfinite(da_var["latitude"]) & np.isfinite(da_var["longitude"])
+    da_var = da_var.where(valid.compute(), drop=True)
+
+    return da_var
 
 #---------------
 
