1b242698e99c47be1e275e5a656671f609a98750
Jesse Turner <jturner@jpss-cloud6.cira.colostate.edu>
2026-04-14 21:12:11 +0000

    Switched to wavenumber

--- 04-02_modtran_IR_cloud_compare.py
+++ 04-02_modtran_IR_cloud_compare.py
@@ -7,14 +7,14 @@ def main():
     modtran_inversion = "data/modtran/modtran_json/04-02_inversion.json"
     modtran_clear = "data/modtran/modtran_json/04-02_clear_sky.json"
 
-    wl1, Tb1 = get_modtran_Tb(modtran_low_cloud)
-    wl2, Tb2 = get_modtran_Tb(modtran_inversion)
-    wl3, Tb3 = get_modtran_Tb(modtran_clear)
+    wl1, freq1, Tb1 = get_modtran_Tb(modtran_low_cloud)
+    wl2, freq2, Tb2 = get_modtran_Tb(modtran_inversion)
+    wl3, freq3, Tb3 = get_modtran_Tb(modtran_clear)
 
     Tb_lines = [
-        {"wl": wl1, "Tb": Tb1, "label": "Low Cloud", "color": "blue"},
-        {"wl": wl2, "Tb": Tb2, "label": "Inversion", "color": "red"},
-        {"wl": wl3, "Tb": Tb3, "label": "Clear Sky", "color": "green"},
+        {"wl": wl1, "freq": freq1, "Tb": Tb1, "label": "Low Cloud", "color": "blue"},
+        {"wl": wl2, "freq": freq2, "Tb": Tb2, "label": "Inversion", "color": "red"},
+        {"wl": wl3, "freq": freq3, "Tb": Tb3, "label": "Clear Sky", "color": "green"},
     ]
     
     plot_Tb_spectra(Tb_lines)
@@ -26,7 +26,8 @@ def get_modtran_Tb(modtran_json_path):
     modtran_df1 = m_utils.open_tp7_file("flc_custom1.tp7")
     modtran_df2 = m_utils.open_7sc_file("flc_custom1.7sc")
     wl, Tb = 10000/modtran_df2['FREQ'], modtran_df2['BBODY_T[K]']
-    return wl, Tb
+    freq = modtran_df2['FREQ']
+    return wl, freq, Tb
 
 def plot_Tb_spectra(Tb_lines):
     plt.style.use('dark_background')
@@ -34,12 +35,12 @@ def plot_Tb_spectra(Tb_lines):
     ax.set_facecolor('black')
 
     for line in Tb_lines:
-        ax.plot(line["wl"], line["Tb"], label=line["label"], color=line["color"], linewidth=0.5,)
+        ax.plot(line["freq"], line["Tb"], label=line["label"], color=line["color"], linewidth=1,)
     
-    ax.set_xlim((3,12))
-    ax.set_ylim((180,300))
+    # ax.set_xlim((3,12))
+    ax.set_ylim((200,300))
 
-    ax.set_xlabel("Wavelength (μm)")
+    ax.set_xlabel("Wavenumber (cm$^{-1}$)")
     ax.set_ylabel("Brightness Temperature (K)")
     ax.set_title("MODTRAN IR spectra")
     ax.legend()
